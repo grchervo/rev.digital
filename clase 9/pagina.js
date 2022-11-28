c84a1c90b919c4af20347c0c02ab2f61bc30db0b
grchervo <118400928+grchervo@users.noreply.github.com>
2022-11-27 23:16:12 -0300

    Add files via upload

---
+++ clase 9/pagina.js
@@ -0,0 +1,14 @@
+<script>
+    {alert("hello world!!")}
+</script>
+
+var pais = "España";
+var continente = "Europa";
+var antiguedad = "2021";
+var pais_y_continente = pais + "" + continente;
+
+pais = "Mexico";
+continente = "Latinoamerica";
+
+console.log(pais, continente, antiguedad);
+alert(pais_y_continente);
